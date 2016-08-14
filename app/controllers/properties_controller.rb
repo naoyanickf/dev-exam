f23cfc3c0f96237cf0bad5013209cd495d63693b
fujisyan <wfknaoya0731@gmail.com>
2016-08-14 10:41:33 +0900

    fix UPDATE method

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -29,6 +29,24 @@ class PropertiesController < ApplicationController
     @stations = Station.where(property_id: @property.id)
   end
 
+  def update
+    @property = Property.find(params[:id])
+    @property.update(name: params["name"], price: params["price"], address: params["address"], age: params["age"], detail: params["detail"])
+    @property.save
+
+    stations = Station.where(property_id: @property.id)
+    @first_station = stations[0]
+    @second_station = stations[1]
+
+    @first_station.update(property_id: @property.id, line_name: params["line_name_1"], name:  params["name_1"], distance:  params["distance_1"])
+    @first_station.save
+    
+    @second_station.update(property_id: @property.id, line_name: params["line_name_2"], name:  params["name_2"], distance:  params["distance_2"])
+    @second_station.save
+
+    redirect_to @property
+  end
+
   def destroy
     @property = Property.find(params[:id])
     @property.delete
